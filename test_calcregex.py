a748685d1ce773c3bb79dc810640e802219b8b29
unknown <mrcsptr@gmail.com>
2018-10-11 17:25:05 +0200

    added test calc_regex

---
+++ test_calcregex.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+import unittest
+from calc_regex import *
+
+
+class MyFirstTests(unittest.TestCase):
+
+	def testcalc(self):
+		self.assertEqual(score_calc('calc som 5 et 9'), 14)
+
+
+
+
+
+
